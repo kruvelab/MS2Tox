1d75267676c86ec51b8d8b25b9a8ab8aff76d4e0
Drew Szabo <103048720+drewszabo@users.noreply.github.com>
2023-08-04 13:36:13 +0200

    Update MS2Tox_github.R

--- R/MS2Tox_github.R
+++ R/MS2Tox_github.R
@@ -153,62 +153,64 @@ FingerPrintTable <- function(subfolder, fp_names_pos, fp_names_neg, fp_names_com
 
 #' @export
 FingerPrintTablePOS <- function(subfolder, folderwithSIRIUSfiles){
+  
   fingerprint_data <- tibble()
 
   if (length(subfolder) > 0 ){
-  # progress bar
-  ii = 1
-  print("Reading in positive mode SIRIUS files ...")
-  pb <- txtProgressBar(min = 0, max = length(subfolder), initial = 0, style = 3)
+    # progress bar
+    ii = 1
+    print("Reading in positive mode SIRIUS files ...")
+    pb <- txtProgressBar(min = 0, max = length(subfolder), initial = 0, style = 3)
 
-  for(direct in subfolder){
-# subfolder with data must be on a form where id is on second place after nr_ (0_Einc270001_Einc270001)
-    file_name <- str_split(direct, "_")
-    comp_name <- str_split(direct, "/")
+    for(direct in subfolder){
+      # subfolder with data must be on a form where id is on second place after nr_ (0_Einc270001_Einc270001)
+      file_name <- str_split(direct, "_")
+      comp_name <- str_split(direct, "/")
 
-    sir_fold <- file_name[[1]][1]
+      sir_fold <- file_name[[1]][1]
 
-    # detect patRoon featureID and parse to output
-    if (str_detect(file_name[[1]][2], "M") & str_detect(file_name[[1]][3], "R")) {
-      id_this <- str_c(file_name[[1]][2], file_name[[1]][3], file_name[[1]][4], sep = "_")
-    } else id_this <- file_name[[1]][2]
+      # detect patRoon featureID and parse to output
+      if (str_detect(file_name[[1]][2], "M") & str_detect(file_name[[1]][3], "R")) {
+        id_this <- str_c(file_name[[1]][2], file_name[[1]][3], file_name[[1]][4], sep = "_")
+      } else id_this <- file_name[[1]][2]
     
-    pred_ion <- comp_name[[1]][3]
-    # changed from readr::read_delim to utils::read.delim due to verbose output
-    filedata <- read.delim(paste(folderwithSIRIUSfiles, direct, sep = "/"), sep = " ", header = FALSE)
-    filedata <- as_tibble(t(filedata))
-    filedata <- filedata %>%
-      mutate(predion = pred_ion) %>%
-      mutate(predion = sub("\\..*", "", predion)) %>%
-      mutate(id = id_this) %>%
-      mutate(sir_fol_nr = sir_fold) %>%
-      mutate(predform = sub("\\_.*", "", predion))
-    fingerprint_data <- fingerprint_data %>%
-      bind_rows(filedata)
+      pred_ion <- comp_name[[1]][3]
+      # changed from readr::read_delim to utils::read.delim due to verbose output
+      filedata <- read.delim(paste(folderwithSIRIUSfiles, direct, sep = "/"), sep = " ", header = FALSE)
+      filedata <- as_tibble(t(filedata))
+      filedata <- filedata %>%
+        mutate(predion = pred_ion) %>%
+        mutate(predion = sub("\\..*", "", predion)) %>%
+        mutate(id = id_this) %>%
+        mutate(sir_fol_nr = sir_fold) %>%
+       mutate(predform = sub("\\_.*", "", predion))
+      fingerprint_data <- fingerprint_data %>%
+        bind_rows(filedata)
 
-    # update progress bar
-    setTxtProgressBar(pb, ii)
-    ii = ii + 1
+      # update progress bar
+      setTxtProgressBar(pb, ii)
+      ii = ii + 1
     
-    }
-  Sys.sleep(1)
-  close(pb)
-  return(fingerprint_data)
-}
+      }
+    Sys.sleep(1)
+    close(pb)
+    return(fingerprint_data)
   } else return fingerprint_data
+}
 
 #' @export
 FingerPrintTableNEG <- function(subfolder, folderwithSIRIUSfiles){
+  
   fingerprint_data <- tibble()
 
   if (length(subfolder) > 0){
-  # progress bar
-  ii = 1
-  print("Reading in negative mode SIRIUS files ...")
-  pb <- txtProgressBar(min = 0, max = length(subfolder), initial = 0, style = 3)
+    # progress bar
+    ii = 1
+    print("Reading in negative mode SIRIUS files ...")
+    pb <- txtProgressBar(min = 0, max = length(subfolder), initial = 0, style = 3)
   
-  for(direct in subfolder){
-# subfolder with data must be on a form where id is on second place after nr_ (0_Einc270001_Einc270001)
+    for(direct in subfolder){
+      # subfolder with data must be on a form where id is on second place after nr_ (0_Einc270001_Einc270001)
       file_name <- str_split(direct, "_")
       comp_name <- str_split(direct, "/")
 
@@ -241,8 +243,8 @@ FingerPrintTableNEG <- function(subfolder, folderwithSIRIUSfiles){
   Sys.sleep(1)
   close(pb)
   return(fingerprint_data)
-}
-  } else return(fingerprint_data)
+} else return(fingerprint_data)
+  }
 
 #' @export
 SiriusScoreRank1 <- function(subfolder_score, folderwithSIRIUSfiles){
